f69647955d1afabed5a705bc3378e256e1ea79d1
Stanislav Lapshin <sm.lapshin@gmail.com>
2021-07-22 15:34:39 +0300

    Refactor command template

--- internal/core/components_test.go
+++ internal/core/components_test.go
@@ -13,7 +13,7 @@ func TestPrepareCommand(t *testing.T) {
 		want    []string
 	}{
 		{
-			[]string{"/bin/bash", "-c", "./deploy --tag=${arg_tag}"},
+			[]string{"/bin/bash", "-c", "./deploy --tag={{.Args.tag}}"},
 			map[string]string{
 				"tag":     "124",
 				"command": "rm -rf /",
@@ -32,11 +32,14 @@ func TestPrepareCommand(t *testing.T) {
 	}
 
 	for _, testCase := range tests {
-		testname := fmt.Sprintf("%s,%s", testCase.command, testCase.args)
-		t.Run(testname, func(t *testing.T) {
-			command := prepareCommand(testCase.command, testCase.args)
-			if !reflect.DeepEqual(command, testCase.want) {
-				t.Errorf("got %s, want %s", command, testCase.want)
+		t.Run(fmt.Sprintf("%s,%s", testCase.command, testCase.args), func(t *testing.T) {
+			command, err := prepareCommand(testCase.command, testCase.args)
+			if err != nil {
+				t.Errorf("err: %s", err)
+			} else {
+				if !reflect.DeepEqual(command, testCase.want) {
+					t.Errorf("got %s, want %s", command, testCase.want)
+				}
 			}
 		})
 	}
